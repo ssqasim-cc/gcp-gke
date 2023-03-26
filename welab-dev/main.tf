5fa6f5f80740710a94a1c6c91ae22b98ca9f0b5d
Syed Safder Qasim <ssqasim@Syed-Safder-Qasim.local>
2023-03-26 22:48:33 +0500

    inital iac code

---
+++ welab-dev/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+	google = {
+	  source = "hashicorp/google"
+	  version = "3.51.0"
+	}
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file_path)
+
+  project = var.project_id
+  region  = var.region
+  zone    = var.main_zone
+}
+
+module "google_networks" {
+  source = "../module/network"
+
+  project_id = var.project_id
+  region     = var.region
+}
+
+module "google_kubernetes_cluster" {
+  source = "../module/kubernetes_cluster"
+
+  project_id                 = var.project_id
+  cluster_name               = var.cluster_name
+  machine_type               = var.machine_type
+  region                     = var.region
+  node_zones                 = var.cluster_node_zones
+  service_account            = var.service_account
+  network_name               = module.google_networks.network.name
+  subnet_name                = module.google_networks.subnet.name
+  master_ipv4_cidr_block     = module.google_networks.cluster_master_ip_cidr_range
+  pods_ipv4_cidr_block       = module.google_networks.cluster_pods_ip_cidr_range
+  services_ipv4_cidr_block   = module.google_networks.cluster_services_ip_cidr_range
+  authorized_ipv4_cidr_block = "${module.bastion.ip}/32"
+}
+
+module "bastion" {
+  source = "../module/bastion"
+
+  project_id   = var.project_id
+  region       = var.region
+  zone         = var.main_zone
+  bastion_name = "app-cluster"
+  network_name = module.google_networks.network.name
+  subnet_name  = module.google_networks.subnet.name
+}
---
+++ welab-dev/variables.tf
@@ -0,0 +1,41 @@
+
+variable "project_id" {
+  type = string
+  description = "The ID of the project to create resources in"
+}
+
+variable "cluster_name" {
+  type = string
+  description = "This will be the clustername"
+}
+
+variable "machine_type" {
+  type = string
+  default = "e2-medium"
+  description = "The zones where worker nodes are located"
+}
+
+variable "region" {
+  type = string
+  description = "The region to use"
+}
+
+variable "main_zone" {
+  type = string
+  description = "The zone to use as primary"
+}
+
+variable "cluster_node_zones" {
+  type = list(string)
+  description = "The zones where Kubernetes cluster worker nodes should be located"
+}
+
+variable "credentials_file_path" {
+  type = string
+  description = "The credentials JSON file used to authenticate with GCP"
+}
+
+variable "service_account" {
+  type = string
+  description = "The GCP service account"
+}
